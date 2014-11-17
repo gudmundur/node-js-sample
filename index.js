00ce213d245e87efd5853fcc22d60430cc3359c9
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2014-11-17 15:04:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello pizza with cheese and eggs!')
+  response.send('Hello pizza with cheese and no eggs!')
 })
 
 app.listen(app.get('port'), function() {
